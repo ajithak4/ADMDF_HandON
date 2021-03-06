c33c004c35ebda4deea425b23153801aa4f1e187
Ajithkumar J <ajithjsa123@gmail.com>
2021-03-06 20:40:40 +0530

    new

---
+++ Asp .Net core/AccountDetails/AccountDetails/Data/AccountDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AccountDetails.Models;
+
+namespace AccountDetails.Data
+{
+    public class AccountDetailsContext : DbContext
+    {
+        public AccountDetailsContext (DbContextOptions<AccountDetailsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AccountDetails.Models.Account> Account { get; set; }
+    }
+}
---
+++ Asp .Net core/AccountDetails/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE Account
+(
+	AccountId INT NOT NULL identity PRIMARY KEY , 
+    AccountName NCHAR(10) NULL
+)
