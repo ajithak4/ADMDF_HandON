5c76911cdc03e475f4541f9076e71ad6d2bf72f7
Ajithkumar J <ajithjsa123@gmail.com>
2021-03-07 20:38:52 +0530

    Logging

---
+++ Asp .Net core/Logging/Logging/Models/CustomFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logging.Models
+{
+    public class CustomFilter : IExceptionFilter
+    {
+
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ViewResult { ViewName = "CustomException" };
+            var modelMetaData = new EmptyModelMetadataProvider();
+            result.ViewData = new ViewDataDictionary(modelMetaData, context.ModelState);
+            result.ViewData.Add("HandleException", context.Exception);
+            context.Result = result;
+            context.ExceptionHandled = true;
+            LoggerManager loggerManager = new LoggerManager();
+            loggerManager.LogInformation("Exception : " + context.Exception.Message);
+        }
+    }
+}
